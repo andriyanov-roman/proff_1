c3a8a00c8cbb901909b615c2b24acd7919e22c3d
Alan <alan_falan@mail.ru>
2015-01-25 12:36:00 +0200

    added alan module

---
+++ alan/src/main/java/except/GoodValidator.java
@@ -0,0 +1,15 @@
+package except;
+public class GoodValidator {
+    public boolean isGoodNameValid(String goodValue) {
+        if (goodValue == null) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isGoodQuantityValid(String goodValue) {
+        if (goodValue == null) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ alan/src/main/java/except/Main.java
@@ -0,0 +1,11 @@
+package except;
+public class Main {
+    public static void main(String[] args) {
+       new View();
+
+    }
+}
+
+
+
+
