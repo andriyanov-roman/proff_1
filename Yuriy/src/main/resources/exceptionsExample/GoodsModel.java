07965bc0f6db81a94c9894390e72f70728fd26c5
Yurceh1 <fobcif2@gmail.com>
2015-01-25 10:55:38 +0200

    yuriy excptions package

---
+++ Yuriy/src/main/resources/exceptionsExample/GoodsModel.java
@@ -0,0 +1,19 @@
+package exceptionsExample;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GoodsModel {
+
+    public static void writeToFile(Goods good) throws IOException {
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("src\\GoodsList.txt", true)));
+            out.println(good);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
