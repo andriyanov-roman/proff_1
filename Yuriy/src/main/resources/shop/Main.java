86b172e6f6c96b048b3c45f43331da2c18f4f4ea
Yurceh1 <fobcif2@gmail.com>
2015-02-19 23:22:44 +0200

    yuriy home tasks added

--- Yuriy/src/main/resources/shop/Main.java
+++ Yuriy/src/main/resources/shop/Main.java
@@ -1,12 +1,11 @@
 package shop;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         new View();
-
-
     }
 }
