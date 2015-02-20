f2a9bacd0ff097b6696b5ea0bd56ebaa8ad1a451
vladislav <gerasimnkovld@gmail.com>
2015-02-20 21:35:00 +0200

    updates

--- vlad/src/main/java/shop/main/Main.java
+++ vlad/src/main/java/shop/main/Main.java
@@ -1,14 +1,8 @@
 package shop.main;
-
-import shop.db_access.GoodDAO;
-import shop.goods.Good;
-
 import java.io.IOException;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(new HashSet<Good>(new GoodDAO().selectAll()).size());
         new View();
     }
 }
