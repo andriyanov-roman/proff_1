99b64912499f7f4643f6313f1b585749dac07e72
Yurceh1 <fobcif2@gmail.com>
2015-02-22 15:38:45 +0200

    yurii new module

--- yurii/src/main/java/shop/dbAccessAndOthersSamples/GoodsModel.java
+++ yurii/src/main/java/shop/dbAccessAndOthersSamples/GoodsModel.java
@@ -1,24 +1,24 @@
-package shop.dbAccessAndOthersSamples;
-
-import shop.Good;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-public class GoodsModel {
-
-    public static void writeToFile(Good good) throws IOException {
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Yuriy/src/main/files/goods.txt", true)));
-            out.println(good);
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-}
+//package shop.dbAccessAndOthersSamples;
+//
+//import shop.Good;
+//
+//import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//public class GoodsModel {
+//
+//    public static void writeToFile(Good good) throws IOException {
+//        try {
+//            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Yuriy/src/main/files/goods.txt", true)));
+//            out.println(good);
+//            out.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//}
