5cd92d2e2006b4219c32b4b6d903cdb159aa3907
Yurceh1 <fobcif2@gmail.com>
2015-02-28 18:19:16 +0200

    yurii

---
+++ yurii/src/main/java/servlets/LoginPasswordServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import shop.User;
+import shop.Validator;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class LoginPasswordServlet extends HttpServlet {
+    Validator validator;
+
+    public void init() throws ServletException {
+        validator = new Validator();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        User u = new User();
+        u.setLogin(login);
+        u.setPasword(password);
+        try {
+            if (validator.isLoginAndPasswordCorrect(u)) {
+                RequestDispatcher view = request.getRequestDispatcher("");
+                view.forward(request, response);
+            } else {
+                RequestDispatcher view = request.getRequestDispatcher("");
+                view.forward(request, response);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ yurii/web/WEB-INF/loginPassword.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<title>Enter Login and Password</title>
+<meta charset="utf-8">
+<body>
+<div class="frm">
+    <form name="loginForm" method="post" action="login">
+        Login:<input type="text" name="login"/> <br/>
+        Password:<input type="password" name="password"/> <br/>
+
+        <div class="btn"><input type="submit" value="Submit" class="button"/></div>
+
+    </form>
+</div>
+</body>
+</html>
