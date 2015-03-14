d2629cb48a05311cf12fa3c71d618c7b100eed83
Alan <alan_falan@mail.ru>
2015-03-14 12:27:28 +0200

    added alan module

---
+++ alan/src/main/java/person/TestCritery.java
@@ -0,0 +1,13 @@
+package person;
+
+import org.hibernate.Session;
+import singletones.HbSessionFactory;
+
+public class TestCritery {
+    Session session = HbSessionFactory.getInstance().getSession();
+@Test
+public void TestRole() throws Exception{
+
+}
+}
+
